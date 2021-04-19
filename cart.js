043cd1c93d4e988a6f64091672eb8d5e2a2b8699
John Lee <77995563+asnow3023@users.noreply.github.com>
2021-04-19 17:18:59 -0400

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,107 @@
+
+let list;
+let listAdd;
+let itemCount;
+let totalPrice;
+
+let email = sessionStorage.getItem('email'); //gets the users email from sessionStorage
+
+getCart(email);
+
+function getCart($email) {
+    $.ajax({
+        url: Url + 'GetCart',
+        type: 'get',
+        dataType: 'json',
+        data: {"email":$email},
+        contentType: 'text/plain',
+        success: function (data) {
+
+            list = '';
+            listAdd = '';
+            itemCount = 0;
+            totalPrice = 0;
+
+            $.each(data['data']['List'], function (i, item) {
+                listAdd = '<div class="row main align-items-center">\n' +
+                    '                        <div class="col-2"><img class="img-fluid" src="' + item['image'] + '"></div>\n' +
+                    '                        <div class="col">\n' +
+                    '                            <div class="row text-muted">' + item['operating_system'] + '</div>\n' +
+                    '                            <div class="row">' + item['title'] + '</div>\n' +
+                    '                        </div>\n' +
+                    '                        <div class="col"> <a class="border">1</a></div>\n' +
+                    '                        <div class="col">&dollar; ' + item['money_price'] + ' <a onclick="deleteItem(' + item['id'] + ')" type="button">&#10005;</a></div>\n' +
+                    '                    </div>';
+                list = list + listAdd;
+                itemCount++;
+                totalPrice += parseInt(item['money_price']);
+            });
+
+            $('#cart-list').html(list);
+            $('#item-count').html(itemCount + ' items');
+            $('#item-total').html(itemCount + ' items');
+            $('#item-price').html('&dollar; ' + totalPrice);
+
+        },
+        error: function (data) {
+            alert("Error while fetching data.");
+        }
+    });
+}
+
+function sortCart(){
+
+$.ajax({
+        url: Url + 'SortCart',
+        type: 'get',
+        dataType: 'json',
+        data: {"email":$email},
+        contentType: 'text/plain',
+        success: function (data) {
+
+            list = '';
+            listAdd = '';
+            sortedList = '';
+            priorityList = '';
+            laterList = '';
+            itemCount = 0;
+            totalPrice = 0;
+            arraylist = data['data']['List'];
+
+            arraylist.sort(function (a,b){
+                $.each(arraylist, function (i,item)){
+                    if(a['title'].localeCompare(b['title']) == -1
+                        { return -1;}
+                    else if(a['title'].localeCompare(b['title']) == 1
+                        { return 1;}
+                    else { return 0;
+                }
+            }
+
+            $.each(data['data']['List'], function (i, item) {
+                listAdd = '<div class="row main align-items-center">\n' +
+                    '                        <div class="col-2"><img class="img-fluid" src="' + item['image'] + '"></div>\n' +
+                    '                        <div class="col">\n' +
+                    '                            <div class="row text-muted">' + item['operating_system'] + '</div>\n' +
+                    '                            <div class="row">' + item['title'] + '</div>\n' +
+                    '                        </div>\n' +
+                    '                        <div class="col"> <a class="border">1</a></div>\n' +
+                    '                        <div class="col">&dollar; ' + item['money_price'] + ' <a onclick="deleteItem(' + item['id'] + ')" type="button">&#10005;</a></div>\n' +
+                    '                    </div>';
+                list = list + listAdd;
+                itemCount++;
+                totalPrice += parseInt(item['money_price']);
+            });
+
+            $('#cart-list').html(list);
+            $('#item-count').html(itemCount + ' items');
+            $('#item-total').html(itemCount + ' items');
+            $('#item-price').html('&dollar; ' + totalPrice);
+
+        },
+        error: function (data) {
+            alert("Error while fetching data.");
+        }
+    });
+}
+}
